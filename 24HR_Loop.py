52bd08a0163bf8786515a1b98fcddfbe20e8c43c
Stephen Board <sboard@lion.lmu.edu>
2021-06-10 21:33:58 -0700

    add emailing log data

--- 24HR_Loop.py
+++ 24HR_Loop.py
@@ -1,7 +1,6 @@
 from distribution import Execute
-import subprocess, time
+import subprocess, time, sys, smtplib, ssl
 from datetime import datetime, timedelta
-import sys
 
 def scan(filename):
     config = []
@@ -15,14 +14,16 @@ def scan(filename):
                 config.append([])
             elif "MEMOS" in line:
                 config.append([])
+            elif "EMAIL" in line:
+                config.append([])
             elif "NEXT_TIME" in line:
                 config.append([])
             else:
                 config[len(config)-1].append(line[:-1])
     infile.close()
-    if len(config[4][0]) < 12:
-        config[4][0]+='0'
-    return (config[0],config[1],config[2],config[3],config[4])
+    if len(config[5][0]) < 12:
+        config[5][0]+='0'
+    return (config[0],config[1],config[2],config[3],config[4],config[5])
 
 def test(TOKENS,EXCEPTIONS,POT,MEMOS):
     test = 1/len(TOKENS)
@@ -45,6 +46,14 @@ def increment_time(old_time_str,old_time,filename):
     write_lines = w_file.writelines(read_lines)
     w_file.close()
 
+def broadcast_results(receipt,time,email):
+    context = ssl.create_default_context()
+    port =  465
+    message = "Subject: "+time+" transactions \n\n"+receipt
+    with smtplib.SMTP_SSL("smtp.gmail.com",port,context=context) as server:
+        server.login("krma.backend@gmail.com",email[2])
+        server.sendmail("krma.backend@gmail.com",[email[0],email[1]],message)
+
 def get_time(next_str):
     now = time.strftime("%Y%m%d%H%M")
     next_time_dt = datetime.strptime(next_str, '%Y%m%d%H%M')
@@ -53,7 +62,7 @@ def get_time(next_str):
 
 def main(filename):
     while True:
-        TOKENS,EXCEPTIONS,POT,MEMOS,NEXT_TIME = scan(filename)
+        TOKENS,EXCEPTIONS,POT,MEMOS,EMAIL,NEXT_TIME = scan(filename)
         next,now,now_str = get_time(NEXT_TIME[0])
         print("time is "+str(now)+" and "+POT[0].split(":")[0]+
             " "+POT[0].split(":")[1]+" to distribute at "+str(next))
@@ -64,9 +73,10 @@ def main(filename):
             next,now,now_str = get_time(NEXT_TIME[0])
             print(POT[0].split(":")[0]+" "+POT[0].split(":")[1]+
                 " to distribute in "+str((next-now).total_seconds()/60)+" minutes.")
-        TOKENS,EXCEPTIONS,POT,MEMOS,NEXT_TIME = scan(filename)
+        TOKENS,EXCEPTIONS,POT,MEMOS,EMAIL,NEXT_TIME = scan(filename)
         test(TOKENS,EXCEPTIONS,POT,MEMOS)
-        Execute.reward_holders(TOKENS,EXCEPTIONS,MEMOS,POT,PUB=True)
+        receipt = Execute.reward_holders(TOKENS,EXCEPTIONS,MEMOS,POT,PUB=True)
+        broadcast_results(receipt,NEXT_TIME[0],EMAIL)
         increment_time(NEXT_TIME[0],next,filename)
 
 if len(sys.argv) == 2:
