5ffcbaba7c493fa55a9828c189fe15a364fc5891
Stephen Board <sboard@lion.lmu.edu>
2021-07-10 12:06:25 -0700

    moved email function to distribution

--- distribution.py
+++ distribution.py
@@ -2,8 +2,14 @@
 from Horizon_InterfaceSDB import Functions
 from pandas import DataFrame as DF
 from pandas import set_option
-import time
 from random import randint as rand
+import time
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email.mime.text import MIMEText
+from email import encoders
+import smtplib
+
 set_option('display.max_colwidth', None)
 
 class Execute:
@@ -41,10 +47,40 @@ class Execute:
         log.write(Receipt_String)
         log.close()
 
-        return Receipt_String
+        return Receipt_String,receipt_file
 
+    def check_dist_ballance(info,pub):
+        pub_key = info.split(":")[3]
+        code = info.split(":")[1]
+        issuer = info.split(":")[2]
+        return Functions.get_balance(pub_key,asset=[code,issuer],public=pub)
 
-    def Trade():
+    def post_process(text):
+        return text
+
+    def broadcast_results(receipt,file,time,email,days_left=0):
+        msg = MIMEMultipart()
+        if days_left:
+            subject = "William, only " + str(days_left) + " days balance remains!"
+        else:
+            subject = time + " distribution recap"
+        msg['Subject'] = subject
+        # msg.attach(MIMEText(post_process(receipt)))
+        msg.attach(MIMEText("Recap attached"))
+        attachment = open(file, "rb")
+        p = MIMEBase('application', 'octet-stream')
+        p.set_payload((attachment).read())
+        encoders.encode_base64(p)
+        p.add_header('Content-Disposition', "attachment; filename= %s" % file)
+        msg.attach(p)
+        smtp = smtplib.SMTP("smtp.gmail.com", 587)
+        smtp.starttls()
+        smtp.login(email[0], email[3])
+        text = msg.as_string()
+        smtp.sendmail(email[0],[email[1],email[2]],text)
+        smtp.quit()
+
+    def auto_depositor():
         print("converting donated currencies")
         WISHING_WELLS = [TEST_URTH.wells,TEST_URTH.wells]
         log = ""
